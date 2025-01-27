e766aa15cec54fe3ae0ccbe3fd5ad79f421a75fa
Adrian Garza <adriangarza236@yahoo.com>
2025-01-27 09:53:00 -0600

    completed sequences lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        return print([])
+    elif length == 1:
+        return print([0])
+    elif length == 2:
+        return print([0, 1])
+    else:
+        fib_seq = [0, 1]
+        while len(fib_seq) < length:
+            next_term = fib_seq[-1] + fib_seq[-2]
+            fib_seq.append(next_term)
+        return print(fib_seq)
